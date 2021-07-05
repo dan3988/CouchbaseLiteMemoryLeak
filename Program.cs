3c9db86ea1caabb100d373c8277bc47b8781df1c
daniel.pickett@workmobileforms.com <daniel.pickett@workmobileforms.com>
2021-07-05 17:00:02 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+using Couchbase.Lite;
+
+Couchbase.Lite.Support.NetDesktop.EnableTextLogging("cb.log");
+
+var blob = new Blob("application/x-binary", new byte[20 * 1024 * 1204]);
+var array = new MutableArrayObject();
+var sw = Stopwatch.StartNew();
+
+array.AddBlob(blob);
+
+sw.Stop();
+
+Console.WriteLine(sw.Elapsed);
